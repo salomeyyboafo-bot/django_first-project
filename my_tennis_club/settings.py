cbeacb5e38b9375775c5318a2623ae2b712f243c
Salomey Boafo <salomeyyboafo@gmail.com>
2025-11-19 14:59:14 +0000

    404

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5*6ugp-4vc!9zv12h1ff8rm4@ugrpjak49ns&o##71)2uvh0#o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
