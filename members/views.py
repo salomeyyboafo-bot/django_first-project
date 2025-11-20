3a66908ee0d12d605b32605dc6d0a17712510b6d
Salomey Boafo <salomeyyboafo@gmail.com>
2025-11-20 11:12:28 +0000

    Finished Dajango Admin

--- members/views.py
+++ members/views.py
@@ -20,4 +20,13 @@ def details(request, id):
 
 def main(request):
   template = loader.get_template('main.html')
-  return HttpResponse(template.render(request))
+  return HttpResponse(template.render())
+
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
+  }
+  return HttpResponse(template.render(context, request))
+
+
